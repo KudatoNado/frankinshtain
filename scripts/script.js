4f784629b10943157c1dd22d2ad67716dd8d6fb8
KudatoNado <kodinetsdaniil@gmail.com>
2025-11-06 18:52:43 +0300

    tt
    r

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,55 @@
-const scrollBtn = document.querySelector('.scroll-top');
 
 
-scrollBtn.addEventListener('click', () => {
-  window.scrollTo({
-    top: 0,           
-    behavior: 'smooth' 
-  });
-});
+console.log('ПРивет, мир');
+let message;
+
+message ='hello!';
+
+alert(message);
+
+
+
+let favoriteFood = 'птицца';
+console.log(favoriteFood);
+   favoriteFood = 'суши';
+   console.log(favoriteFood);
+   const planet = "Земля";
+   console.log(planet);
+
+
+
+
+
+const pi = 3.14;
+
+console.log(pi);
+
+let userName;
+let test123;
+
+let $ = 1;
+let P_dor = 2;
+alert($ + P_dor);
+
+let n = 123;
+n = 12.342;
+alert(2);
+alert(1);
+alert (1/0);
+alert (1/0)
+
+
+
+let box = null;
+let anotherBox;
+console.log(box, anotherBox)
+
+let colors = ["красный","зелёный","синий"];
+console.log(colors[0]);
+
+
+let text = "ррррр";
+console.log(typeof text);
+
+let color = "blue";
+console.log(`sdfsefasfwfe - ${color} !`)
