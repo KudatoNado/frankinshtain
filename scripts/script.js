8e28eda3bb0a112b4fcd8275def662a77e231c42
KudatoNado <kodinetsdaniil@gmail.com>
2025-11-13 17:29:50 +0300

    js
    aga

--- scripts/script.js
+++ scripts/script.js
@@ -53,3 +53,169 @@ console.log(typeof text);
 
 let color = "blue";
 console.log(`sdfsefasfwfe - ${color} !`)
+
+
+let buttonPressed = false;
+
+if (buttonPressed) {
+   console.log("кнопка Нажата")
+} 
+
+// let isAdult = true;
+// let isAdult = false;
+
+// 5>3
+// 10===10
+// 7<2
+//  f = a
+//  10 == 3 
+// 10 === g
+
+
+// 51 === "3"
+
+let namber = -10;
+
+if (namber > 0)
+{
+   console.log("Позетивно")
+}
+else
+{
+   if(namber)
+   console.log("отрецательно")
+}
+
+
+let grede = 4;
+if(grede >=9){
+   console.log("Отлично");
+}
+else if(grede >=7){
+   console.log("Хорошо");
+}
+else if(grede >=5){
+   console.log("Удолетворительно");
+}
+else {
+   console.log("Нужно подкочаться");
+}
+
+
+let рр = 4;
+if(рр >=30){
+   console.log("Жара");
+}
+else if(рр >=20){
+   console.log("тепло");
+}
+else if(рр >=10){
+   console.log("Прохладно");
+}
+else {
+   console.log("Холодно");
+}
+
+
+let age = 20;
+let hasTicket = true;
+ if (age >= 18 && hasTicket)
+ { console.log("Добро");}
+ else if (age >= 18 && !hasTicket)
+ {
+   console.log("Пошел от сюда шегол");
+ }
+else{
+   console.log ("Шелб ты от сюда");
+}
+
+
+let day = 3
+switch (day) {
+   case 1:
+      console.log("Понедельник");
+      break;
+         case 2:
+            console.log("Вторник");
+            break;
+            case 3:
+               console.log("Среда");
+               break;
+               case 4:
+                  console.log("Четверг");
+                  break;
+                  case 5:
+                     console.log("Пятница");
+                     break;
+                     case 6:
+                        console.log("Субота");
+                        break;
+                        case 7:
+                           console.log("Воскресенье");
+                           break;
+      default:
+         console.log("это что за день?");
+         
+                           
+}
+
+let krto = "admin"
+switch (krto) {
+   case "admin":
+      console.log("O я тебя знаю");
+      break;
+         case "ghost":
+            console.log("AAAAAAAAAAAAA");
+            break;
+            case "user":
+               console.log("Ладно");
+               break;
+              
+      default:
+         console.log("Кто ты");
+}
+
+
+let is0line = true;
+let status = is0line ? "В сети" : "Не в сети";
+console.log("status:", status);
+
+// let is0line = true;
+// let status = is0line ? "ГОтов" : "не готов";
+// console.log("status:", status);
+
+
+for (let i = 1 ; i <= 5; i++) {
+   console.log("Повтор " + i); 
+}
+
+for (let i = 5 ; i <= 15; i++) {
+   console.log(i); 
+}
+
+for (let i = 5 ; i <= 15; i++) {
+   console.log(Math.random()); 
+}
+
+let i = 1;
+
+while (i <= 5)
+{
+   console.log("Шаг" + i);
+   i++
+}
+
+let maney = 110;
+
+while (maney>0){
+   console.log("Покупвю мороженое!");
+   maney -= 20;
+}
+
+let b=10
+while ( b > 0){
+   console.log(b);
+   b--
+}
+
+
